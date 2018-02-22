d73dae13a3981d74b4756ccf3596a01f49098b92
krabsen <krabsenm@gmail.com>
2018-02-22 13:58:08 +0100

    Initial commit, cover job created plus dummy test

---
+++ DoorControl/DoorControl.Test.Unit/DoorControlUnitTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DoorControl.Test.Unit
+{
+    public class DoorControlUnitTests
+    {
+        [Test]
+        public void IsDoorOpen_testsState_ReturnsFalse()
+        {
+            var uut = new DoorControl();
+          
+            Assert.That(uut.IsDoorOpen(), Is.EqualTo(false));
+        }
+    }
+}
---
+++ DoorControl/DoorControl/DoorControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorControl
+{
+    public class DoorControl
+    {
+        private bool _isDoorOpen;
+
+        public DoorControl()
+        {
+            _isDoorOpen = false;
+        }
+
+        public bool IsDoorOpen()
+        {
+            return _isDoorOpen; 
+        }
+    }
+}
