d74d31c9d37737a240a9c3b49b6e1c09d9e6f504
TobiasDN <tobias.dalgaard.nielsen@post.au.dk>
2018-02-22 15:57:40 +0100

    Alarm and EntryNotification implemented

---
+++ DoorControl/Alarm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoorControl
+{
+    class Alarm : IAlarm
+    {
+        public void SignalAlarm()
+        {
+            Console.WriteLine("Door Breached *BWEEP bip bip BWEEP*");
+        }
+    }
+}
